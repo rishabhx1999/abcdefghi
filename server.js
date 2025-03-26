fde85ce34b7bb8a1bb6b36b21c3b183a75dbe81d
Danish Bansal <danishbansal60@gmail.com>
2025-03-26 22:56:03 +0530

    added allowed origin another

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const itemRoutes = require('./routes/item');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['https://ecommerce-site-danish.netlify.app','https://abcdefghi-357aalqga-rishabh-bansals-projects-738eae69.vercel.app'];
+const allowedOrigins = ['https://ecommerce-site-danish.netlify.app',
+    'https://abcdefghi-357aalqga-rishabh-bansals-projects-738eae69.vercel.app',
+    'https://abcdefghi-nine.vercel.app'];
 app.use(cors({
     origin : function(origin, callback){
         if(allowedOrigins.indexOf(origin) !== -1 || !origin){
